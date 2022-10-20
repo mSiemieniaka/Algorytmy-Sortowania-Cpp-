bd8a67621735f1109bcad469e5fdb69154121364
Marcin Siemieniaka <74233241+mSiemieniaka@users.noreply.github.com>
2022-10-20 16:50:20 +0200

    Add files via upload

---
+++ Aisd Projekt 2 Haszowanie kod cpp.cpp
@@ -0,0 +1,174 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <chrono>
+#include <ctime>
+#include <cstdlib>
+#include <random>
+
+using namespace std;
+
+int licznik = 0;
+
+void wypełnianie(int* tablica, int m)
+{
+    for (int i = 0; i <= (m - 1); i++)
+    {
+        tablica[i] = (-1);
+    }
+}
+
+int h1(int x, int m)
+{
+    return x % m;
+}
+int h2(int x, int m) 
+{
+    return(((x / m) % (m / 2)) * 2) + 1;
+}
+
+int h(int x, int i, int m) 
+{
+    return (h1(x, m) + i * h2(x, m)) % m;
+}
+
+bool hash_Wstaw_podwojnie(int* tablica, int m, int x)
+{
+    
+    int k;
+    int h1;
+    int h2;
+    for (int i = 0; i < m; i++)
+    {
+        h1 = x % m;
+        h2 = (((x / m) % (m / 2)) * 2) + 1;
+        k = (h1 + i * h2) % m;
+        if (tablica[k] == -1)
+        {
+            tablica[k] = x;
+            licznik++;
+            return true;
+        }
+        licznik++;
+    }
+    return false;
+}
+
+
+bool hash_al_wstaw(int* tablica, int m, int x)
+{
+    int k;
+    int h;
+    for (int i = 0; i < m; i++)
+    {
+        h = x % m;
+        k = (h + i) % m;
+        if (tablica[k] == -1)
+        {
+            tablica[k] = x;
+            licznik++;
+            return true;
+      }
+        licznik++;
+    }
+
+    return false;
+}
+
+
+bool hash_al_szukaj(int* tablica, int m, int x)
+{
+    for (int i = 0; i < m - 1; i++)
+    {
+        int k = h(x,i,m);
+        licznik++;
+
+
+        if (tablica[k] == x)
+        {
+            licznik++;
+            return true;
+            
+        }
+
+        if (tablica[k] == -1)
+        {
+            licznik++;
+            return false;
+
+        }
+
+    }
+
+    return false;
+}
+bool hash_al_szukaj_mieszaj(int* tablica, int m, int x)
+{
+    int k;
+    int h1;
+    int h2;
+    for (int i = 0; i < m - 1; i++) {
+        h1 = x % m;
+        h2 = (((x / m) % (m / 2)) * 2) + 1;
+        k = (h1 + i * h2) % m;
+        if (tablica[k] = x) 
+        {
+            licznik++;
+            return true;
+        }
+        else if (tablica[k] == -2) 
+        {
+            licznik++;
+            return false;
+        }
+    }
+    return false;
+}
+
+void miszanie(int* tablica, int l)
+{
+    for (int i = 0; i < l; i++)
+    {
+        tablica[i] = i;
+    }
+    random_shuffle(tablica, tablica + l);
+
+}
+
+void czas_mierz(int* tablica, int rozmiar, int* tab_wym, int element) {
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = element; i < element + 10000; i++)
+    {
+       //hash_al_szukaj(tablica, rozmiar, tab_wym[i]);
+        //hash_Wstaw_podwojnie(tablica, rozmiar, tab_wym[i]);
+        //hash_wstaw(tablica, rozmiar, tab_wym[i]);
+        hash_al_szukaj_mieszaj(tablica, rozmiar, tab_wym[i]);
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::micro> duration = end - start;
+    std::cerr << "Uplynelo: " << duration.count() << "us\n" << "wykonano " << licznik << " razy";
+}
+int main()
+{
+    int element10000;
+    int m = 1048576;
+    int l = 12000000;
+    int dzp = 0;
+    int* tablica = new int[m]; // tablica mieszająca
+    int* tablica_miesz = new int[l]; // tablica z danymi
+    wypełnianie(tablica, m);
+    miszanie(tablica_miesz, l);
+
+
+    for (int a = 0; a < m; a++)
+    {
+        //hash_wstaw(tablica, m, tablica_miesz[a]);
+        //hash_al_szukaj(tablica, m, tablica_miesz[a]);
+        hash_al_szukaj_mieszaj(tablica, m, tablica_miesz[a]);
+        //hash_Wstaw_podwojnie(tablica, m, tablica_miesz[a]);
+        if (a >= m * 0)
+        {
+            czas_mierz(tablica, m, tablica_miesz, a);
+            break;
+        }
+    }
+}
---
+++ Aisd sortowania z losowymi danymi.cpp
@@ -0,0 +1,263 @@
+﻿#include <iostream>
+#include <chrono>
+#include <random>
+#include <limits>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include <fstream>
+#include <clocale>
+
+using std::swap;
+
+
+int losowa_liczba(int min, int max)
+{
+	static std::default_random_engine gen(std::random_device{}());
+	static std::uniform_int_distribution<int> dist;
+	return dist(gen, std::uniform_int_distribution<int>::param_type{ min, max });
+}
+
+void wypelnij(int* tablica, int rozmiar, int min = 0, int max = std::numeric_limits<int>::max())
+{
+	for (int i = 0; i < rozmiar; ++i)
+		tablica[i] = losowa_liczba(min, max);
+}
+
+bool jest_posortowane(int* tablica, int rozmiar)
+{
+	return std::is_sorted(tablica, tablica + rozmiar);
+}
+
+double mierz_czas(int* tablica, int rozmiar, void(*funkcja_sortujaca)(int*, int))
+{
+	auto start = std::chrono::high_resolution_clock::now();
+	funkcja_sortujaca(tablica, rozmiar);
+	auto end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> duration = end - start;
+	return duration.count();
+}
+
+/*
+
+	parametry:
+	 - funkcja_sortujaca - wskanik na funkcjącą sortowanie, musi przyjmować dwa parametry: adres początku tablicy (int*) oraz jej rozmiar (int)
+	 - nazwa - nazwa testowanej funkcji, tylko w celach wypisania
+	 - output - strumien do ktorego beda zapisane wyniki, domyslnie std::cerr, przy ostatnim uruchomieniu warto nadpisac otwartym strumieniem plikowym, aby sobie zebrac wyniki do pliku
+	 - dodatkowe_miejsce - liczba dodatkowych elementow tablicy zaalokowanych PRZED poczatkiem tablicy, przykladowo gdy =1, pierwszym indeksem tablicy jest -1, ale dane rozpoczynaja sie od indeksu 0, moze sie przydac do sortowania przez wstawianie z wartownikiem
+*/
+void eksperyment(void(*funkcja_sortujaca)(int*, int), const std::string& nazwa, std::ostream& output = std::cerr, int dodatkowe_miejsce = 0)
+{
+	//ustawienia
+	const double limit_czasu = 30.0; //sekund
+	const int powtorzen = 3;
+	const int rozmiar_poczatkowy = 1 << 10;
+	/////////////////////////////////////////
+	const int szerokosc = 100;
+	int gwiazdek = szerokosc - nazwa.length() - 2;
+	if (gwiazdek < 0)
+		gwiazdek = 0;
+	int i = 0;
+	output << " ";
+	for (; i < gwiazdek / 2; ++i)
+		output << '*';
+	output << " " << nazwa << " ";
+	for (; i < gwiazdek; ++i)
+		output << '*';
+	output << "\n\n";
+	output.flush();
+
+	output << std::setw(9) << "N";
+	output << std::setw(1) << "";
+	for (int nr = 0; nr < powtorzen; ++nr)
+		output << std::setw(9) << nr + 1 << " ";
+	output << std::setw(12) << "średna" << " ";
+	output << "\n";
+	for (int rozmiar = rozmiar_poczatkowy; ; rozmiar *= 2)
+	{
+		output << std::setw(9) << rozmiar << ": ";
+		output.flush();
+		int* pamiec = new int[dodatkowe_miejsce + rozmiar];
+		int* tablica = pamiec + dodatkowe_miejsce;
+		double czas = 0.0;
+
+		int* pattern = new int[rozmiar];
+
+
+		for (int nr = 0; nr < powtorzen; ++nr)
+		{
+			wypelnij(tablica, rozmiar);
+			for (int i = 0; i < rozmiar; ++i)
+				pattern[i] = tablica[i];
+			std::sort(pattern, pattern + rozmiar);
+			double c = mierz_czas(tablica, rozmiar, funkcja_sortujaca);
+			if (!jest_posortowane(tablica, rozmiar))
+			{
+				output << "Tablica nieposortowana!!\n";
+				if (&output != &std::cerr)
+					std::cerr << "Tablica nieposortowana!!\n";
+				return;
+			}
+			if (!std::equal(pattern, pattern + rozmiar, tablica, tablica + rozmiar))
+			{
+				output << "Tablica zawiera inne wartosci niz powinna!!\n";
+				if (&output != &std::cerr)
+					std::cerr << "Tablica zawiera inne wartosci niz powinna!!\n";
+				return;
+			}
+			czas += c;
+			output.precision(6);
+			output << std::fixed << c << "  ";
+			output.flush();
+		}
+		czas /= powtorzen;
+		output << std::setw(12) << std::fixed << czas << "\n";
+		output.flush();
+		delete[] pamiec;
+		delete[] pattern;
+		if (czas > limit_czasu)
+			break;
+	}
+	output << "\n";
+	output.flush();
+}
+void sortowanie_przez_wstawianie(int* tablica, int rozmiar)
+{
+	int pom;
+	int j;
+	for (int i = 1; i < rozmiar; i++)
+	{
+		pom = tablica[i];
+		j = i - 1;
+
+		while (j >= 0 && tablica[j] > pom)
+		{
+			tablica[j + 1] = tablica[j];
+			--j;
+		}
+		tablica[j + 1] = pom;
+	}
+}
+
+
+	void sortowanie_przezWybieranie(int* tablica, int rozmiar)
+	{
+
+
+		for (int i = 0; i <= (rozmiar - 1); i++)
+		{
+			int k = i;
+			int x = tablica[i];
+
+			for (int j = i + 1; j <= (rozmiar - 1); j++)
+			{
+				if (tablica[j] < x)
+				{
+					k = j;
+					x = tablica[j];
+				}
+			}
+			tablica[k] = tablica[i];
+			tablica[i] = x;
+		}
+
+	}
+	void sortowanie_babelkowe(int* tablica, int rozmiar)
+	{
+		for (int i = 0; i < rozmiar; i++)
+		{
+			for (int j = 1; j < rozmiar - i; j++)
+			{
+				if (tablica[j - 1] > tablica[j])
+				{
+					swap(tablica[j - 1], tablica[j]);
+				}
+			}
+		}
+	}
+	void sortowanie_shella(int* tablica, int rozmiar)
+	{
+		int h = 1;
+		while (h < (rozmiar / 9))
+		{
+			h = 3 * h + 1;
+		}
+		while (h > 0)
+		{
+			for (int i = h; i < rozmiar; i++)
+			{
+				int x = tablica[i]; int j = i;
+				while ((j >= h) && (x < tablica[j - h]))
+				{
+					tablica[j] = tablica[j - h]; j = j - h;
+				}
+				tablica[j] = x;
+			}
+			h = h / 3;
+		}
+	}
+	void sortowanie_szybkie(int* tablica, int d, int g)
+{
+	if (d < g) {
+		int t = tablica[d];
+		int s = d;
+		for (int i = d; i <= g; i++) {
+			if (tablica[i] < t) {
+				s = s + 1;
+				swap(tablica[s], tablica[i]);
+			}
+		}
+		swap(tablica[d], tablica[s]);
+		sortowanie_szybkie(tablica, d, s - 1);
+		sortowanie_szybkie(tablica, s + 1, g);
+	}
+}
+void sortowanie_szybkie_start(int* tablica, int rozmiar) 
+{
+	sortowanie_szybkie(tablica, 0, rozmiar - 1);
+}
+void stdsort(int* tablica, int rozmiar)
+{
+	std::sort(tablica, tablica + rozmiar);
+}
+void sortowanie_szybkie_modyf(int* tablica, int d, int g)
+{
+	if (d < (g - 10))
+	{
+		int t = tablica[d];
+		int s = d;
+		for (int i = d; i <= g; i++)
+		{
+			if (tablica[i] < t)
+			{
+				s = s + 1;
+				swap(tablica[s], tablica[i]);
+			}
+		}
+		swap(tablica[d], tablica[s]);
+		sortowanie_szybkie_modyf(tablica, d, s - 1);
+		sortowanie_szybkie_modyf(tablica, s + 1, g);
+	}
+}
+void sortowanie_szybkie_start_modyf(int* tablica, int rozmiar)
+{
+	sortowanie_szybkie_modyf(tablica, 0, rozmiar - 1);
+
+	sortowanie_przez_wstawianie( tablica,  rozmiar);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	std::ofstream wyniki("wyniki.txt");
+
+	std::ostream& output = std::cerr; //zmienic na = wyniki aby zapisywalo do pliku
+	eksperyment(sortowanie_szybkie_start_modyf,  "Sortowanie Szybkie zmodyfikowane", output);
+	eksperyment(sortowanie_przez_wstawianie, "Sortowanie przez wstawianie", output);
+	eksperyment(stdsort, "Sortowanie std::sort", output);
+	eksperyment(sortowanie_szybkie_start, "Sortowanie szybkie", output);
+	eksperyment(sortowanie_shella, "Sortowanie shella", output);
+	eksperyment(sortowanie_babelkowe, "Sortowanie babelkowe", output);
+	eksperyment(sortowanie_przezWybieranie, "Sortowanie przez wybieranie", output);
+	return 0;
+}
